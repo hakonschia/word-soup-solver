615bec3011c083a82e998adffc6d2f6906e27269
Håkon Schia <hakonschia5@gmail.com>
2021-08-06 10:12:30 +0200

    Convert project to Compose Desktop

--- solver/src/main/kotlin/Main.kt
+++ solver/src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
+import androidx.compose.desktop.Window
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import soup.SoupSolver
 
 const val EXIT_MESSAGE = "-1"
 
+/*
 fun main() {
     val board = arrayOf(
         charArrayOf('E', 'S', 'L', 'F', 'V', 'J', 'C', 'Y', 'Ø', 'K', 'E', 'T', 'T', 'P'),
@@ -55,4 +66,49 @@ fun main() {
     } while (word != null && word != EXIT_MESSAGE)
 
     println("\n\n\nExiting, final board:\n${solver.generateBoard()}")
+}
+ */
+
+@OptIn(ExperimentalFoundationApi::class)
+fun main() = Window {
+    val board = arrayOf(
+        charArrayOf('E', 'S', 'L', 'F', 'V', 'J', 'C', 'Y', 'Ø', 'K', 'E', 'T', 'T', 'P'),
+        charArrayOf('R', 'N', 'E', 'S', 'P', 'O', 'M', 'S', 'W', 'P', 'Z', 'B', 'D', 'A'),
+        charArrayOf('E', 'O', 'T', 'Q', 'W', 'S', 'Y', 'U', 'T', 'L', 'Ø', 'A', 'G', 'M'),
+        charArrayOf('A', 'M', 'I', 'M', 'F', 'Ø', 'D', 'S', 'N', 'O', 'G', 'P', 'S', 'D'),
+        charArrayOf('T', 'F', 'S', 'S', 'Ø', 'Z', 'S', 'E', 'T', 'N', 'K', 'I', 'F', 'B'),
+        charArrayOf('Å', 'C', 'F', 'Æ', 'P', 'D', 'N', 'J', 'O', 'I', 'V', 'U', 'Z', 'E'),
+        charArrayOf('T', 'N', 'T', 'V', 'R', 'N', 'R', 'S', 'L', 'T', 'L', 'D', 'Æ', 'R'),
+        charArrayOf('I', 'E', 'S', 'N', 'E', 'E', 'T', 'O', 'W', 'E', 'V', 'T', 'U', 'M'),
+        charArrayOf('A', 'G', 'R', 'V', 'N', 'I', 'G', 'R', 'F', 'R', 'F', 'Å', 'E', 'E'),
+        charArrayOf('Y', 'R', 'S', 'I', 'K', 'E', 'S', 'E', 'Z', 'G', 'S', 'K', 'N', 'S'),
+        charArrayOf('B', 'O', 'F', 'E', 'D', 'R', 'P', 'V', 'N', 'E', 'D', 'Y', 'K', 'Æ'),
+        charArrayOf('L', 'M', 'R', 'W', 'F', 'Ø', 'X', 'H', 'E', 'W', 'T', 'D', 'H', 'V'),
+        charArrayOf('K', 'E', 'T', 'J', 'F', 'V', 'Æ', 'K', 'C', 'I', 'Z', 'G', 'S', 'D'),
+        charArrayOf('O', 'T', 'Ø', 'M', 'S', 'U', 'N', 'D', 'D', 'P', 'S', 'G', 'J', 'L')
+    )
+
+    val solver = SoupSolver(board)
+
+    LazyVerticalGrid(cells = GridCells.Fixed(board.size)) {
+        items(count = board.size * board.size) { pos ->
+            val row = pos / board.size
+            val column = pos % board.size
+
+            println("row=$row, column=$column")
+
+
+            Card(
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = Color.Gray
+                )
+            ) {
+                Text(
+                    text = board[row][column].uppercase(),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
 }
