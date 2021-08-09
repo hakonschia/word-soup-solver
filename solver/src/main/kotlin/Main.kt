3e64239db17882454ba0f08340f2f5d6e4d9b087
Håkon Schia <hakonschia5@gmail.com>
2021-08-09 09:48:41 +0200

    Add arrow for downwards diagonal

--- solver/src/main/kotlin/Main.kt
+++ solver/src/main/kotlin/Main.kt
@@ -1,23 +1,27 @@
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import soup.SoupSolver
 import soup.SoupWordSolution
 import soup.WordDirection
 import util.SizeDp
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * The size of a cell in the board
@@ -120,10 +124,11 @@ fun main() = Window {
 fun BoardCell(board: Array<CharArray>, row: Int, column: Int) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = Modifier
             .height(CELL_SIZE)
             .width(CELL_SIZE)
-            .padding(CELL_PADDING)
+           // .padding(CELL_PADDING)
             .border(
                 width = 1.dp,
                 color = Color.Gray
@@ -142,11 +147,6 @@ fun BoardCell(board: Array<CharArray>, row: Int, column: Int) {
  */
 @Composable
 fun SolutionArrow(solution: SoupWordSolution) {
-    val isDiagonally = when (solution.direction) {
-        WordDirection.HORIZONTAL, WordDirection.HORIZONTAL_REVERSE, WordDirection.VERTICAL, WordDirection.VERTICAL_REVERSE -> false
-        WordDirection.DIAGONAL_DOWN, WordDirection.DIAGONAL_DOWN_REVERSE, WordDirection.DIAGONAL_UP, WordDirection.DIAGONAL_UP_REVERSE -> true
-    }
-
     val size = when (solution.direction) {
         WordDirection.HORIZONTAL, WordDirection.HORIZONTAL_REVERSE -> {
             SizeDp(width = CELL_SIZE * solution.word.length, height = CELL_SIZE)
@@ -157,46 +157,82 @@ fun SolutionArrow(solution: SoupWordSolution) {
             SizeDp(width = CELL_SIZE, height = CELL_SIZE * solution.word.length)
         }
 
-        // TODO
-        WordDirection.DIAGONAL_DOWN, WordDirection.DIAGONAL_DOWN_REVERSE -> TODO()
-        WordDirection.DIAGONAL_UP, WordDirection.DIAGONAL_UP_REVERSE-> TODO()
+        // This is pythagoras
+        WordDirection.DIAGONAL_DOWN, WordDirection.DIAGONAL_DOWN_REVERSE, WordDirection.DIAGONAL_UP, WordDirection.DIAGONAL_UP_REVERSE -> {
+            val cathetus = (CELL_SIZE * solution.word.length).value.pow(2)
+            val hypotenuse = sqrt(cathetus + cathetus)
+
+            SizeDp(width = hypotenuse.dp, height = CELL_SIZE)
+        }
     }
 
     val rotation = when (solution.direction) {
-        WordDirection.HORIZONTAL, WordDirection.HORIZONTAL_REVERSE -> 0f
-        WordDirection.VERTICAL, WordDirection.VERTICAL_REVERSE -> 0f
-        WordDirection.DIAGONAL_DOWN, WordDirection.DIAGONAL_DOWN_REVERSE -> 45f
-        WordDirection.DIAGONAL_UP, WordDirection.DIAGONAL_UP_REVERSE-> 315f
+        WordDirection.HORIZONTAL, WordDirection.HORIZONTAL_REVERSE -> 0
+        WordDirection.VERTICAL, WordDirection.VERTICAL_REVERSE -> 0
+        WordDirection.DIAGONAL_DOWN -> 45
+        WordDirection.DIAGONAL_DOWN_REVERSE -> 135
+        WordDirection.DIAGONAL_UP, WordDirection.DIAGONAL_UP_REVERSE-> 315
     }
 
     // first = x, second = y
     val paddings = when (solution.direction) {
-        WordDirection.HORIZONTAL -> 0.dp to 0.dp
         // It's just offset backwards horizontally
         WordDirection.HORIZONTAL_REVERSE -> -CELL_SIZE * (solution.word.length - 1) to 0.dp
-        WordDirection.VERTICAL -> 0.dp to 0.dp
         WordDirection.VERTICAL_REVERSE -> 0.dp to -CELL_SIZE * (solution.word.length - 1)
 
-        // TODO
-        WordDirection.DIAGONAL_DOWN -> 0.dp to 0.dp
-        WordDirection.DIAGONAL_DOWN_REVERSE -> 0.dp to 0.dp
-        WordDirection.DIAGONAL_UP -> 0.dp to 0.dp
-        WordDirection.DIAGONAL_UP_REVERSE -> 0.dp to 0.dp
+        // For the rotation to end up at the correct place we need to offset it as well
+        // The rotation point is at the halfway on Y, so that's why we offset by half the size (see graphicsLayer code below)
+        // since the rotation point will be stationary
+        WordDirection.DIAGONAL_DOWN -> 0.dp to -CELL_SIZE * 0.5f
+
+        else -> 0.dp to 0.dp
+    }
+
+
+    val x = CELL_SIZE * solution.startCoordinates.x + paddings.first
+    val y = CELL_SIZE * solution.startCoordinates.y + paddings.second
+
+    val animatedProgress = remember { Animatable(0f) }
+    LaunchedEffect(animatedProgress) {
+        animatedProgress.animateTo(
+            rotation.toFloat(),
+            animationSpec = tween(
+                durationMillis = 2500,
+                delayMillis = 250
+            ))
     }
 
     Column(
         modifier = Modifier
-            // The paddings are basically the coordinates we start at (start = x, top = y)
-            .padding(
-                start = CELL_SIZE * solution.startCoordinates.x + paddings.first,
-                top = CELL_SIZE * solution.startCoordinates.y + paddings.second
-            )
             .height(size.height)
             .width(size.width)
-            .rotate(rotation)
+            .offset(x, y)
+            .graphicsLayer {
+                if (rotation != 0) {
+                    // The transform origin determines where the rotation is done. Default is at the center (0.5f, 0.5f)
+                    // The rotation point will be stationary, so where we want the rotation point to end is where the start it
+
+                    // (0, 0.5f) means all the way to the left and halfway down, 0% on x and 50% on y
+                    // I.e., the rotation point is at the star:
+                    // - - - -
+                    // *     |
+                    // - - - -
+
+
+                    transformOrigin = TransformOrigin(0f, 0.5f)
+                    rotationZ = animatedProgress.value
+                }
+            }
             .border(
                 width = 2.dp,
-                color = Color.WordSolution
-            )
-    ) { }
+                color = Color.WordSolution,
+                //shape = RoundedCornerShape(50)
+            ),
+
+        // This can be removed when the text is removed, as it's just a visual helper for development
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "•", textAlign = TextAlign.Center)
+    }
 }
